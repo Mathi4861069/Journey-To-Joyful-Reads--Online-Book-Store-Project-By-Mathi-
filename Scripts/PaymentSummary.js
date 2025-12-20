d41d73a0ba6f194c7126866047dc66f303ef1846
Mathi4861069 <mathi4861069@gmail.com>
2025-12-20 12:35:51 +0530

    calculate the payment summary with js

--- Scripts/PaymentSummary.js
+++ Scripts/PaymentSummary.js
@@ -31,8 +31,44 @@ export function renderPaymentSummary()
 
     });
 
-    totalCartPrice = changeformat(totalCartPrice);
+    const TotalBeforeTax = eval( totalCartPrice + shippingCost);
+    const taxCalculation = TotalBeforeTax *0.05;  // we multiple 0.05 for 5% of tax//
+    const TotalAfterTax = eval(TotalBeforeTax+taxCalculation);
 
     console.log("Total Book Price" + totalCartPrice);
-    console.log("Total shipping cost" + changeformat(shippingCost));
+    console.log("Total " + TotalBeforeTax);
+    console.log(shippingCost);
+    console.log(taxCalculation);
+    console.log(TotalAfterTax);
+
+
+    const paymentHTML=`
+        <table class="table">
+
+                <tr>
+                    <td> Cart Books Total Price Rs:</td>
+                    <td>${changeformat (totalCartPrice)}</td>
+                </tr>
+                <tr>
+                    <td> Shipping Price Rs:</td>
+                    <td>${changeformat (shippingCost)}</td>
+                </tr>
+                <tr>
+                    <td>Total Price Before Tax :</td>
+                    <td>${changeformat (TotalBeforeTax)}</td>
+                </tr>
+                <tr>
+                    <td>Extimated Tax (5%):</td>
+                    <td>${changeformat (taxCalculation)}</td>
+                </tr>
+                <tr class="total-css">
+                    <td>Total Price With Tax:</td>
+                    <td>${changeformat(TotalAfterTax)}</td>
+                </tr>
+            </table>   
+    `;
+
+    document.querySelector('.js-payment-detail').innerHTML = paymentHTML;
+    renderPaymentSummary();
+ 
 }
